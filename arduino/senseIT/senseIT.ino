93ab13ac0b4ec7716dea2f51f7b440b92e475e97
fsmiamoto <shigueo.miamoto@gmail.com>
2019-02-15 14:54:27 -0200

    Adicionada biblioteca para JSON

--- arduino/senseIT/senseIT.ino
+++ arduino/senseIT/senseIT.ino
@@ -1,52 +1,47 @@
 /*
-   Web Server
+senseIT - Projeto de monitoramento de temperatura, umidade e presença de gás
+com comunicação via JSON.
 
-   A simple web server that shows the value of the analog input pins.
-   using an Arduino Wiznet Ethernet shield.
+Baseado no exemplo WebServer da biblioteca Ethernet
 
-Circuit:
- * Ethernet shield attached to pins 10, 11, 12, 13
- * Analog inputs attached to pins A0 through A5 (optional)
-
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
- modified 02 Sept 2015
- by Arturo Guadalupi
-
+Autor: Francisco Miamoto
+GitHub: /fsmiamoto
  */
 
 #include <SPI.h>
 #include <Ethernet.h>
 #include <DHT.h>
 #include <LiquidCrystal.h>
+#include <ArduinoJson.h>
 
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
-byte mac[] = {
-    0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
-};
+// Definições de rede:
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192, 168, 100, 200);
-
+// DHT 11
 const int dhtPin = 30; // Pino do sensor DHT
-
 DHT dht(dhtPin, DHT11); // Objeto do sensor de temperatura
-LiquidCrystal lcd(34,36,38,46,42,44); // Objeto do LCD
-
-// Initialize the Ethernet server library
-// with the IP address and port you want to use
-// (port 80 is default for HTTP):
+// MQ-2
+const int gasPin = 10; // Pino analógico
+// LCD
+LiquidCrystal lcd(34,36,38,46,42,44); 
+// Inicia servidor na porta 80 (Padrão HTTP)
 EthernetServer server(80);
+// JSON para os dados dos sensores
+StaticJsonBuffer<200> jsonBuffer;
+JsonObject& dados = jsonBuffer.createObject();
 
 void setup() {
+    // Inicialização dos sensores e display
     dht.begin();
     lcd.begin(20, 4);
+    // Imprime texto base no LCD
     lcd.setCursor(0, 0);
     lcd.print("Temperatura: ");
     lcd.setCursor(0, 1);
     lcd.print("    Umidade: ");
-    // Open serial communications and wait for port to open:
+    lcd.setCursor(0, 2);
+    lcd.print("        Gas: ");
+    // Inicia porta serial
     Serial.begin(9600);
     while (!Serial) {
         ; // wait for serial port to connect. Needed for native USB port only
@@ -75,10 +70,14 @@ void setup() {
 
 
 void loop() {
-    
+    // Obtém leitura dos sensores 
     const int temp = (int) dht.readTemperature();
     const int hum = (int) dht.readHumidity();
-
+    const int gas = analogRead(gasPin);
+    // Atualiza JSON
+    dados["temp"] = temp;
+    dados["hum"] = hum;
+    dados["gas"] = gas;
     // Imprime temperatura
     lcd.setCursor(13, 0);
     lcd.print(temp); 
@@ -88,6 +87,10 @@ void loop() {
     lcd.setCursor(13, 1);
     lcd.print(hum);
     lcd.print("%");
+    // Imprime gas
+    lcd.setCursor(13, 2);
+    //TODO: Não atualizar demais
+    lcd.print("25");
 
     // listen for incoming clients
     EthernetClient client = server.available();
@@ -110,12 +113,7 @@ void loop() {
                     client.println("Connection: close");  // the connection will be closed after completion of the response
                     client.println("Refresh: 5");  // refresh the page automatically every 5 sec
                     client.println();
-                    client.print("{\"temp\": ");
-                    client.print(temp);
-                    client.println(",");
-                    client.print("\"hum\": ");
-                    client.print(hum);
-                    client.println("}");
+                    dados.prettyPrintTo(client); // Envia JSON para cliente
                     break;
                 }
                 if (c == '\n') {
