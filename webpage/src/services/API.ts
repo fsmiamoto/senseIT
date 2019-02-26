56079848f81f7c27a259f936ef7ec3546bf62959
fsmiamoto <shigueo.miamoto@gmail.com>
2019-02-25 22:23:35 -0300

    Ajuste para nova rota no Arduino

--- webpage/src/services/API.ts
+++ webpage/src/services/API.ts
@@ -2,7 +2,7 @@ const ARDUINO_URL = "http://192.168.100.200";
 
 export const getMedidas = async () => {
     try {
-        const response = await fetch(ARDUINO_URL);
+        const response = await fetch(`${ARDUINO_URL}/sensors`);
         return await response.json();
     } catch (e) {
         return e;
